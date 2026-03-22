ff894253c2556a6a79a726dccd3d8f97de71ae14
Anshul Rajput <anshulmogha50@gmail.com>
2026-03-22 21:29:26 +0530

    fix: syntex

--- Middelware.js
+++ Middelware.js
@@ -1,8 +1,9 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
-const SECRET = "mysecretkey";
+
 
 function auth(req ,res ,next) {
-    const header = req.headers.authorization;
+    const header = req.headers.authorization ;
 
     if(!header){
         return res.status(401).json({message :"token missing"});
@@ -11,7 +12,7 @@ function auth(req ,res ,next) {
     const token = header.split(" ")[1];
 
     try{
-        const decode = jwt.verify(token ,SECRET);
+        const decode = jwt.verify(token ,process.env.SECRET);
         req.user = decode;
         next();
 
