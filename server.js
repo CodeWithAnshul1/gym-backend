896924bb39d361e642ca276e42d67d0ade135590
Anshul Rajput <anshulmogha50@gmail.com>
2026-05-08 11:53:47 +0530

    allow to cors

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ const app = express();
 
 // ✅ Middleware
 app.use(cors({
-  origin :  "http://localhost:5173",
-  // origin: "https://anshulgymhub.netlify.app",
+  // origin :  "http://localhost:5173",
+  origin: "https://anshulgymhub.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
